9c65a6cc4cd9ca0f1ca965c01e6a6189effd7244
sabrina mianne <sabrina.mianne@gmail.com>
2019-04-05 16:03:34 -0700

    change the range to a string to be able to modify each element

--- js/scripts.js
+++ js/scripts.js
@@ -5,48 +5,37 @@ $(document).ready(function() {
     var number_user = $("input#userInput").val();
 
 //Back end logic
-//basic logic
-var number = 200
-function range(start, end) {
-    var arrayResult = [];
-    for (let i = start; i <= end; i++) {
-        arrayResult.push(i);
-    }
-    return arrayResult;
-}
 
-console.log(range(0,number))
-
-//Back end logic
-
-
-var ex = [1,2,3]
+var ex1= ["1"]
+var ex2 = ["2"]
+var ex3 = ["3"]
+var numbers3 = ["0","1","2","3","4","5","6","7","8","9"]
+var numbers2 = ["0","1","2","4","5","6","7","8","9"]
+var numbers = ["0","1","4","5","6","7","8","9"]
 var exception1 = ["Beep!","Boop!","I'm sorry, Dave. I'm afraid I can't do that."];
 var arrayResult = [];
+var el= "";
+var empty = "";
 
 
-function range(start, end) {
-    for (let i = start; i <= end; i++) {
+function range(number) {
+    for (let i = 0; i <= number; i++) {
         arrayResult.push(i);
         }
-        for (let elt of arrayResult) {
-         console.log(elt)
-         elt2 = elt.slice(0,-1)
-         if (ex.includes(elt2)) {
-   		 	 var indexnumberexc= ex.indexOf(elt)
-       	 var replaceSentence = exception1[indexnumberexc]
-         arrayResult[elt] = replaceSentence
+        el= arrayResult.toString()
+        for (let elt of el) {
+         if (ex1.includes(elt) && numbers.includes(elt)) {
+         el= el.replace(elt, "Beep!")
+         } if (ex2.includes(elt) && numbers2.includes(elt) )  {
+         el = el.replace(elt, "Boop!")
+   		   } if (ex3.includes(elt) && numbers3.includes(elt))  {
+         el = el.replace(ex3, "I'm sorry, Dave. I'm afraid I can't do that.")
+
          }
         }
 
+        return el;
+   }
+console.log(range(20))
 
-        return arrayResult;
-        }
-  /*   for (let elt of arrayResult) {
-      if (arrayResult.includes(ex)){
-
-    }
-    }  */
-
-
-console.log(range(6,13))
+       
