2f849976f250db77f99cc20194349efefd2604bc
sabrina mianne <sabrina.mianne@gmail.com>
2019-04-06 09:40:02 -0700

    modify code logic for three against 2 and 1 and 2 against 1

--- js/scripts.js
+++ js/scripts.js
@@ -9,36 +9,34 @@ $(document).ready(function() {
 var ex1= ["1"]
 var ex2 = ["2"]
 var ex3 = ["3"]
-var numbers3 = ["0","1","2","3","4","5","6","7","8","9"]
-var numbers2 = ["0","1","2","4","5","6","7","8","9"]
-var numbers = ["0","1","4","5","6","7","8","9"]
-var exception1 = ["Beep!","Boop!","I'm sorry, Dave. I'm afraid I can't do that."];
 var arrayResult = [];
-var el= "";
-var empty = "";
+var arrayResult2 = [];
 
 
 function range(number) {
     for (let i = 0; i <= number; i++) {
         arrayResult.push(i);
-        }
-        el= arrayResult.toString()
-        for (let elt of el) {
-         if (ex1.includes(elt) && numbers.includes(elt)) {
-         el= el.replace(elt, "Beep!")
-         } if (ex2.includes(elt) && numbers2.includes(elt) )  {
-         el = el.replace(elt, "Boop!")
-   		   } if (ex3.includes(elt) && numbers3.includes(elt))  {
-         el = el.replace(ex3, "I'm sorry, Dave. I'm afraid I can't do that.")
+        istr= i.toString()
+        if (istr.includes(ex3)) {
+         arrayResult2.push(istr)
+        arrayResult2[istr]=" I'm sorry, Dave. I'm afraid I can't do that. "
+        } else if  (istr.includes(ex2)  ) {
+        arrayResult2.push(istr)
+        arrayResult2[istr]=" Boop! "
 
-         }
+        } else if (istr.includes(ex1)) {
+          arrayResult2.push(istr)
+        arrayResult2[istr]=" Beep! "
+        } else {
+         arrayResult2.push(istr)
+        }
+        }
+        return arrayResult2
         }
 
-        return el;
-   }
+
    $("#finalResult").text(range(number_user));
    $("#hidden").show();
-
 
 });
 });
